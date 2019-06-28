7f0b3480538efdad869c47f7548d62143a3000a2
Mahesh <mahesh.deploy@gmail.com>
2019-06-27 21:46:45 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import HelloWorld from './containers/HelloWorld';
+import Counter from './containers/Counter';
 
 function App() {
 	return (
 		<div className='App'>
 			<header className='App-header'>
-				<HelloWorld />
+				<Counter />
 			</header>
 		</div>
 	);
